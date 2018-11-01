9c100d689ddbffbbc1372b638e7a558d3440c463
zhongbinbi <zhongbinqi@txtechnologies.com>
2018-11-01 17:43:09 +0800

    11.1-pm-17.43

--- parent/parent-microservice/parent-microservice-bank/common-microservice-bank-abcchina/src/main/java/app/controller/ControllerABCChinaAop.java
+++ parent/parent-microservice/parent-microservice-bank/common-microservice-bank-abcchina/src/main/java/app/controller/ControllerABCChinaAop.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.xvolks.jnative.exceptions.NativeException;
-
 import com.crawler.bank.json.BankJsonBean;
 import com.crawler.bank.json.BankStatusCode;
 import com.microservice.dao.entity.crawler.bank.basic.TaskBank;
