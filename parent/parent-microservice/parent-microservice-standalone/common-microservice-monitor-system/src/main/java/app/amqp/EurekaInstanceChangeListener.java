60532158c6f403bb98a82044430a72d0df3a7316
linansun <15501151766@163.com>
2018-11-02 17:00:39 +0800

    微服务上线和断线事件代码完成，可以正常发送通知邮件

--- parent/parent-microservice/parent-microservice-standalone/common-microservice-monitor-system/src/main/java/app/amqp/EurekaInstanceChangeListener.java
+++ parent/parent-microservice/parent-microservice-standalone/common-microservice-monitor-system/src/main/java/app/amqp/EurekaInstanceChangeListener.java
@@ -1,6 +1,9 @@
 package app.amqp;
 
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
@@ -21,12 +24,18 @@ public class EurekaInstanceChangeListener {
 		String microEventType="";
 		tracer.addTag("monitor-system Received", eurekaInstanceBean.toString()); 
 		String eventType = eurekaInstanceBean.getEventType().trim();
+		//将时间戳转成指定格式的时间
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		//加八个小时
+	    Date date = new Date(eurekaInstanceBean.getTimestamp()+8 * 60 * 60 * 1000);
+	    String eventTime = simpleDateFormat.format(date);
+	    
 		if(eventType.contains("CanceledEvent")){  //服务断线事件
 			microEventType="断线事件";
-			eurekaInstanceChangeMailService.sendResultMail(eurekaInstanceBean, microEventType);
+			eurekaInstanceChangeMailService.sendResultMail(eurekaInstanceBean,microEventType,eventTime);
 		}else{  //服务注册  RegisteredEvent //服务上线事件
 			microEventType="注册事件";
-			eurekaInstanceChangeMailService.sendResultMail(eurekaInstanceBean, microEventType);
+			eurekaInstanceChangeMailService.sendResultMail(eurekaInstanceBean, microEventType,eventTime);
 		}
 	}
 }
