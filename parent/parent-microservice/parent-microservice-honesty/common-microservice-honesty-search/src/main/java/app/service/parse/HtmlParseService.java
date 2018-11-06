3f7e47a655bccf15e0c046f5838d936804d858d2
hanyixing <1120546503@qq.com>
2018-11-06 15:36:12 +0800

    三网 到 去掉触发mysql关键字

--- parent/parent-microservice/parent-microservice-honesty/common-microservice-honesty-search/src/main/java/app/service/parse/HtmlParseService.java
+++ parent/parent-microservice/parent-microservice-honesty/common-microservice-honesty-search/src/main/java/app/service/parse/HtmlParseService.java
@@ -111,7 +111,7 @@ public class HtmlParseService extends AbstractParser {
 			
 			if (searchTask.getKeyword() != null) {
 
-				newsListJson.setKey(searchTask.getKeyword());
+				newsListJson.setKeyword(searchTask.getKeyword());
 			}
 			if (searchTask.getTaskid() != null) {
 
@@ -187,7 +187,7 @@ public class HtmlParseService extends AbstractParser {
 
 			if (searchTask.getKeyword() != null) {
 
-				newsListJson.setKey(searchTask.getKeyword());
+				newsListJson.setKeyword(searchTask.getKeyword());
 			}
 			if (searchTask.getTaskid() != null) {
 
@@ -268,7 +268,7 @@ public class HtmlParseService extends AbstractParser {
 
 			if (searchTask.getKeyword() != null) {
 
-				newsListJson.setKey(searchTask.getKeyword());
+				newsListJson.setKeyword(searchTask.getKeyword());
 			}
 			if (searchTask.getTaskid() != null) {
 
