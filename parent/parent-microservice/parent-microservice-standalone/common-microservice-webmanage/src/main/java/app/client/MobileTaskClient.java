ace5a89f76ee7e8b5263108e51971de4e4366d9c
manyizeng <zengmy318@outlook.com>
2018-11-09 17:18:24 +0800

    修改运营商task微服务名称 MobileCrawlerTask ----》mobile-task

--- parent/parent-microservice/parent-microservice-standalone/common-microservice-webmanage/src/main/java/app/client/MobileTaskClient.java
+++ parent/parent-microservice/parent-microservice-standalone/common-microservice-webmanage/src/main/java/app/client/MobileTaskClient.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.crawler.PageInfo;
 import com.microservice.dao.entity.crawler.mobile.TaskMobile;
 
-@FeignClient("MobileCrawlerTask")
+@FeignClient("mobile-task")
 //@FeignClient(name = "mobileTask", url = "http://10.167.202.203:8000")
 public interface MobileTaskClient {
 
